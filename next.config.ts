79ee19030d20c05135638b7aef69e142323d7049
wildanmkhdev <wildankhalidun97@gmail.com>
2025-06-09 18:47:36 +0700

    fixing bug

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false, // matikan strict mode kalau ada warning strict
-	swcMinify: true, // gunakan SWC untuk minify, default Next.js terbaru
 	eslint: {
-		ignoreDuringBuilds: true, // supaya build gak gagal karena eslint error
-	},
-	typescript: {
-		ignoreBuildErrors: true, // supaya build lanjut meski ada error TS (hati-hati)
-	},
-	experimental: {
-		appDir: true, // pastikan pakai App Router
+		ignoreDuringBuilds: true,
 	},
 };
 
