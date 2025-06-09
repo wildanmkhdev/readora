3cfd7024d44271b5e47896463a45f0a3d14256d6
wildanmkhdev <wildankhalidun97@gmail.com>
2025-06-10 00:08:41 +0700

    upd

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,17 +1,17 @@
-// /app/category/[slug]/page.tsx
-"use client";
-import { useParams } from "next/navigation";
+// // /app/category/[slug]/page.tsx
+// "use client";
+// import { useParams } from "next/navigation";
 
-const CategoryDetailPage = () => {
-	const params = useParams();
-	const { slug } = params;
+// const CategoryDetailPage = () => {
+// 	const params = useParams();
+// 	const { slug } = params;
 
-	return (
-		<div className="p-10 h-screen">
-			<h1 className="text-3xl font-bold capitalize">Category: {slug}</h1>
-			{/* Di sini bisa fetch data berdasarkan slug jika perlu */}
-		</div>
-	);
-};
+// 	return (
+// 		<div className="p-10 h-screen">
+// 			<h1 className="text-3xl font-bold capitalize">Category: {slug}</h1>
+// 			{/* Di sini bisa fetch data berdasarkan slug jika perlu */}
+// 		</div>
+// 	);
+// };
 
-export default CategoryDetailPage;
+// export default CategoryDetailPage;
