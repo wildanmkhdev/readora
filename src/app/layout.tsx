bf3483fc6bbf64faa2141e6ae2bc3d99775cb49b
wildanmkhdev <wildankhalidun97@gmail.com>
2025-06-11 22:56:51 +0700

    wkwkwk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,6 +37,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="id">
+			<style>
+				@import
+				url('https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik+Spray+Paint&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+			</style>
 			<body>
 				<Navbar /> {/* Navbar yang akan selalu ada */}
 				<main>{children}</main> {/* Tempat konten halaman akan diganti */}
