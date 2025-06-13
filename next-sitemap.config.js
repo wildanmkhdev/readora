779005cc5a3ecc63d05934782adc286e7e967449
wildanmkhdev <wildankhalidun97@gmail.com>
2025-06-13 15:56:15 +0700

    asss

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.readdora.web.id", // Ganti dengan domain kamu
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "daily",
+  priority: 0.7,
+};
