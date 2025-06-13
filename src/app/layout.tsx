5518c819cb81206927f70e315985d681c3cc457c
wildanmkhdev <wildankhalidun97@gmail.com>
2025-06-13 15:25:27 +0700

    asuu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,20 +16,20 @@ export const metadata = {
 	keywords: ["buku", "ebook", "toko buku", "buku digital", "readoora"],
 	authors: [{ name: "Readoora Team", url: "https://readoora.com" }],
 	creator: "Readoora",
-	metadataBase: new URL("https://readoora.com"),
+	metadataBase: new URL("https://readdora.web.id"),
 	openGraph: {
 		title: "Readoora - Toko Buku Online",
 		description: "Temukan buku terbaik dan ebook favoritmu di Readoora.",
-		url: "https://readoora.com",
-		siteName: "Readoora",
+		url: "https://readdora.web.id",
+		siteName: "Readdora",
 		locale: "id_ID",
 		type: "website",
 	},
 	twitter: {
 		card: "summary_large_image",
-		title: "Readoora - Toko Buku Online",
-		description: "Temukan buku terbaik dan ebook favoritmu di Readoora.",
-		creator: "@readoora",
+		title: "Readdora - Toko Buku Online",
+		description: "Temukan buku terbaik dan ebook favoritmu di Readdora.",
+		creator: "@readdora",
 	},
 	icons: {
 		icon: "/favicon.ico",
@@ -39,42 +39,56 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang="id">
-			<Head>
+			<head>
 				<link
 					href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik+Spray+Paint&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
 					rel="stylesheet"
 				/>
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-			</Head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+			</head>
 
 			<body>
-				<Navbar /> {/* Navbar yang akan selalu ada */}
+				<Navbar />
 				<Script
 					id="chat-widget"
 					strategy="afterInteractive"
 					dangerouslySetInnerHTML={{
 						__html: `
-      window.mychat = window.mychat || {};
-                          window.mychat.server = 'https://live.cekat.ai/widget.js';
-                          window.mychat.iframeWidth = '400px';
-                          window.mychat.iframeHeight = '700px';
-                          window.mychat.accessKey = 'Readdora-IETmfjiY';
-                          (function() {
-                              var mychat = document.createElement('script');
-                              mychat.type = 'text/javascript';
-                              mychat.async = true;
-                              mychat.src = window.mychat.server;
-                              var s = document.getElementsByTagName('script')[0];
-                              s.parentNode.insertBefore(mychat, s);
-                          })();
-    `,
+              window.mychat = window.mychat || {};
+              window.mychat.server = 'https://live.cekat.ai/widget.js';
+              window.mychat.iframeWidth = '400px';
+              window.mychat.iframeHeight = '700px';
+              window.mychat.accessKey = 'Readdora-IETmfjiY';
+              (function() {
+                  var mychat = document.createElement('script');
+                  mychat.type = 'text/javascript';
+                  mychat.async = true;
+                  mychat.src = window.mychat.server;
+                  var s = document.getElementsByTagName('script')[0];
+                  s.parentNode.insertBefore(mychat, s);
+              })();
+            `,
 					}}
 				/>
-				<main>{children}</main> {/* Tempat konten halaman akan diganti */}
-				<Footed /> {/* Footer yang akan selalu ada */}
+				<main>{children}</main>
+				<Footed />
 			</body>
 		</html>
 	);
